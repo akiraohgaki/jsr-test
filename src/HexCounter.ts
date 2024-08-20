ae99207f9edcc1178f685b548db2950369ac2492
Akira Ohgaki <akiraohgaki@gmail.com>
2024-08-20 23:56:36 +0900

    Update HexCounter.ts

--- src/HexCounter.ts
+++ src/HexCounter.ts
@@ -1,28 +1,79 @@
+/**
+ * Count in hexadecimal
+ */
 export default class HexCounter {
   #value = 0;
 
+  /**
+   * Creates a new HexCounter instance.
+   *
+   * @example
+   * ```
+   * const counter = new HexCounter('ff');
+   * ```
+   *
+   * @param value - The initial value of the counter in hexadecimal
+   *
+   * @remarks
+   * If no value is provided, the counter will start at 0.
+   */
   constructor(value?: string) {
     if (value) {
       this.#value = this.#hexToDec(value);
     }
   }
 
+  /**
+   * The current value of the counter in hexadecimal.
+   *
+   * @example
+   * ```
+   * const counter = new HexCounter('ff');
+   * console.log(counter.value); // 'ff'
+   * ```
+   */
   get value(): string {
     return this.#decToHex(this.#value);
   }
 
+  /**
+   * Increments the counter by 1 in hexadecimal.
+   *
+   * @example
+   * ```
+   * const counter = new HexCounter('ff');
+   * counter.increment();
+   * console.log(counter.value); // '100'
+   * ```
+   */
   increment(): void {
     this.#value++;
   }
 
+  /**
+   * Decrements the counter by 1 in hexadecimal.
+   *
+   * @example
+   * ```
+   * const counter = new HexCounter('ff');
+   * counter.decrement();
+   * console.log(counter.value); // 'fe'
+   * ```
+   */
   decrement(): void {
     this.#value--;
   }
 
+  /**
+   * Converts decimal to hexadecimal.
+   */
   #decToHex(dec: number): string {
     return dec.toString(16);
   }
 
+  /**
+   * Converts hexadecimal to decimal.
+   */
   #hexToDec(hex: string): number {
     return parseInt(hex, 16);
   }
