4c8d90e0a9ebee8d949a0a77b027aa063b40b2b7
Akira Ohgaki <akiraohgaki@gmail.com>
2024-09-11 11:29:32 +0900

    Update jsdoc

--- mod.ts
+++ mod.ts
@@ -1,11 +1,11 @@
 /**
+ * @module
+ *
  * Module @akiraohgaki/jsr-test
  *
  * ```ts
  * import * as JsrTest from '@akiraohgaki/jsr-test';
  * ```
- *
- * @module
  */
 
 export { default as HexCounter } from './src/HexCounter.ts';
